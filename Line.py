024496e4966117593ba1c60c606e018b7ff120bd
Ibuki-Bobby-Inomata <44492439+Ibuki-Bobby-Inomata@users.noreply.github.com>
2019-03-14 17:29:52 +0900

    Add files via upload

---
+++ Line.py
@@ -0,0 +1,7 @@
+#LINE送信成功
+def send_OK():
+    return '送信成功'
+#LINE送信失敗
+def send_NG(String):
+    print(String)
+    return '送信成功'
---
+++ Line_Call.py
@@ -0,0 +1,6 @@
+#Lineで書いた関数”send_OK”と”send_NG”の呼び出し
+from Line import send_OK
+from Line import send_NG
+
+print(send_OK())
+print(send_NG('NG'))
