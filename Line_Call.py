8fe8ef7ec0e81d8c9f3fa7117d6736f7b2d362ca
猪股一歩希 <ibuki.i1130bobby@gmail.com>
2020-02-10 12:00:21 +0900

    clean up

--- Line_Call.py
+++
@@ -1,6 +0,0 @@
-#Lineで書いた関数”send_OK”と”send_NG”の呼び出し
-from Line import send_OK
-from Line import send_NG
-
-print(send_OK())
-print(send_NG('NG'))
--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# git-hello-world
-# git-hello-world
-# kokoko
-# kokoko
-# kokoko
-# my_models
 # my_models
